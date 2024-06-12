d120b07d4bf390511d4da7d82fec68431b12a809
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-06-12 12:58:57 -0400

    created Apollo client

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,19 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
 
 /**
  * Create a new apollo client and export as default
  */
+
+// const link = new HttpLink({ uri: 'https://rickandmortyapi.com/graphql' });
+const link = new HttpLink({ uri: 'http://localhost:4000/' });
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
+export default client;
