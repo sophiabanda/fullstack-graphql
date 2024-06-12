e40a4ae20c65846264256ab1e458e76fb0b42690
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-06-12 13:17:26 -0400

    created test query against rickandmorty api, added apollo dev tools

--- client/src/client.js
+++ client/src/client.js
@@ -7,13 +7,26 @@ import gql from 'graphql-tag';
  * Create a new apollo client and export as default
  */
 
-// const link = new HttpLink({ uri: 'https://rickandmortyapi.com/graphql' });
-const link = new HttpLink({ uri: 'http://localhost:4000/' });
+const link = new HttpLink({ uri: 'https://rickandmortyapi.com/graphql' });
+// const link = new HttpLink({ uri: 'http://localhost:4000/' });
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link,
   cache,
 });
+
+const query = gql`
+  query AllCharacters($page: Int, $filter: FilterCharacter) {
+    characters(page: $page, filter: $filter) {
+      results {
+        name
+        id
+      }
+    }
+  }
+`;
+
+client.query({ query }).then((result) => console.log(result));
 
 export default client;
